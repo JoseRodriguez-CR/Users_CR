8ce6067196c9d9fb398adb36009979977d2d5fad
JoseRodriguez-CR <josemariorodriguezc@gmail.com>
2021-09-30 22:22:06 -0600

    Updating Files

--- users.py
+++ users.py
@@ -12,16 +12,15 @@ class User:
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
-        results = connectToMySQL('userscr').query_db(query)
+        results = connectToMySQL('users').query_db(query)
         users = []
         for u in results:
             users.append( cls(u) )
+        print(users)
         return users
 
     @classmethod
     def save(cls, data):
         query = "INSERT INTO users (first_name,last_name,email) VALUES (%(first_name)s,%(last_name)s,%(email)s);"
-
-        # comes back as the new row id
-        result = connectToMySQL('userscr').query_db(query,data)
+        result = connectToMySQL('users').query_db(query,data)
         return result
